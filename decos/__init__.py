5e53331adef41d09ee1b0735e93952667870b05e
Vikas Sangwan <16vikas96@gmail.com>
2019-05-15 14:58:24 +0530

    new release

--- decos/__init__.py
+++ decos/__init__.py
@@ -1,1 +1,5 @@
 name = "decos"
+
+from decos import safe_run, debug, timer
+
+__all__ = ['safe_run','timer','debug']
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md" ,'r') as f:
 
 setuptools.setup(
         name='decos',
-        version='0.0.1',
+        version='0.0.11',
         author='Vikas Sangwan',
         author_email='16vikas96@gmail.com',
         description='some decorators',
