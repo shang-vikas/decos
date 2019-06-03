a29a189c6aedb3c38b696736494de6f0f061aba6
Vikas Sangwan <16vikas96@gmail.com>
2019-06-03 09:28:10 +0530

    release 0.0.12post2

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-This repo hosts 3 useful decorators that might come handy while prototyping
+This repo hosts 3 useful decorators that might come handy while prototyping namely - 
+
+`@debug`
+`@timer`
+`@safe_run`
+
+@debug prints the function name with the arguments passed and the return value at the end each time a function is called.
+
+@timer as the name suggests times the function to be executed and in the end prints the total time taken by that function.
+
+`@safe_run` is in brief tries to run the function and prints the error in case the function throws some error. `@safe_run` takes dictionary of parameters also to let the user customize the behavior of it.
+
+
--- decos/decos.py
+++ decos/decos.py
@@ -1,6 +1,6 @@
 import functools
 import time,os,sys,pdb
-import inspect
+import inspect,traceback
 
 __all__ = ["timer","debug","safe_run"]
 
@@ -26,7 +26,7 @@ def debug(func):
         signature = ", ".join(args_repr + kwargs_repr)           # 3
         print(f"Calling {func.__name__}({signature})")
         value = func(*args, **kwargs)
-        print(f"{func.__name__!r} returned {value!r}")           # 4
+        print(f"{func.__name__!r} returned {value!r} of type - {type(value)}")           # 4
         return value
     return wrapper_debug
 
@@ -35,7 +35,7 @@ def safe_run(_func=None,*,dcs={}):
     """ Tries to run the function and throws the given error(argument)
     dcs['tb']: String to print in try block
     dcs['cb']: String to print in except block
-    dcs['dv']: default value to return: default is None
+    dcs['drv']: default value to return: None
     dcs['var']: var to be printed with cb
     """
     def saferun(func):
@@ -55,28 +55,42 @@ def safe_run(_func=None,*,dcs={}):
         def handle_vars(cb,def_kwargs):
             if not dcs.get('var',None):
                 return cb
-            for var in dcs.get('var'):
+            vars_ = dcs.get('var')
+            for idx,var in enumerate(vars_):
                 if def_kwargs.get(var,None):
                     cb = cb + f' {var} = {str(def_kwargs[var])} '
             return cb
-        
+       
+        def handle_drv(def_kwargs):
+            vals = dcs.get("drv",None)
+            if vals is None: return vals
+            ret_vals = []
+            for val in vals:
+                if isinstance(val,list):
+                    for strr in val: ret_vals.append(def_kwargs.get(strr,None))
+                else: ret_vals.append(val)
+            return ret_vals if ret_vals != [] else ret_vals
+
         def handler(*ar,**kw):
             tb = f"Running the function {func.__name__} " if dcs.get('tb',None) is None else dcs.get('tb')
-            cb = f"Something wrong happened while running {func.__name__} -- " if dcs.get('cb',None) is None else dcs.get('cb')
+            cb = f"Above mentioned error caught while running {func.__name__} -- \nreturning the default values " if dcs.get('cb',None) is None else dcs.get('cb')
             all_ar = get_def_kwargs(*ar,**kw)
             cb = handle_vars(cb,all_ar)
-            return tb,cb
+            drv = handle_drv(all_ar)
+            return tb,cb,drv
 
         @functools.wraps(func)
         def try_run_function(*args,**kwargs):
-            tb,cb = handler(*args,**kwargs)
+            tb,cb,drv = handler(*args,**kwargs)
             try:
                 print(tb)
                 value = func(*args,**kwargs)
                 return value
             except Exception as e:
-                print(cb+f'{e}'+f' More info over here... \n{sys.exc_info()}')
-                return dcs.get('dv',None)
+                _f  = traceback.print_exc()
+                print(cb+f' {e} '+f' More info over here... \n {_f}')
+                del _f
+                return drv
         
         return try_run_function
     
